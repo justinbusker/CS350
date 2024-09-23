48e8c304aaf331b13453f47d0ddd53e6e05b5f6a
Ubuntu <ubuntu@ip-172-31-14-206.us-east-2.compute.internal>
2024-09-23 03:34:50 +0000

    add lab2:

---
+++ Lab02/main.c
@@ -0,0 +1,14 @@
+extern long long int test();
+extern long long int lab02b();
+extern void lab02c(long long int a);
+extern long long int lab02d(long long int a);
+
+int main(void)
+{
+	test();
+	lab02b();
+	lab02c(255);
+	lab02d(4);
+
+    return 0;
+}
